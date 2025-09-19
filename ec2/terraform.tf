aff88cf536d6f215713b1f80e84adf13ab3c6ddd
Anuroop P S <anuroopps2001@gmail.com>
2025-09-19 20:19:56 +0530

    created backend.tf file

--- ec2/terraform.tf
+++ ec2/terraform.tf
@@ -5,17 +5,5 @@ terraform {
       version = "6.13.0"
     }
   }
-
-  # Locking .tfstate file with (s3+dynamodb)
- /*  backend "s3" {
-    bucket         = "remote-backend-bucket-for-storing-statefile"
-    key            = "terraform.tfstate" # object name inside the bucket
-    region         = "us-east-2"
-    dynamodb_table = "remote-backend-table" # for state locking of tfstate file
-  } */
-
-  backend "local" {
-    path = "terraform.tfstate"
-  }
 }
 
