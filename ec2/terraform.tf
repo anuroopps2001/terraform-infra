3ce4ccce46ba9110924984db1282315747230d4a
Anuroop P S <anuroopps2001@gmail.com>
2025-09-19 13:52:50 +0530

    added terraform  reinitialization configuration

--- ec2/terraform.tf
+++ ec2/terraform.tf
@@ -7,11 +7,15 @@ terraform {
   }
 
   # Locking .tfstate file with (s3+dynamodb)
-  backend "s3" {
+ /*  backend "s3" {
     bucket         = "remote-backend-bucket-for-storing-statefile"
     key            = "terraform.tfstate" # object name inside the bucket
     region         = "us-east-2"
     dynamodb_table = "remote-backend-table" # for state locking of tfstate file
+  } */
+
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
 
